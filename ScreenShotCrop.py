229ddf167c618f6b97acae1f13800d2b0b243ca3
kingsuk <kingsuk.majumder@gmail.com>
2019-03-14 23:34:25 +0530

    added source code

---
+++ ScreenShotCrop.py
@@ -0,0 +1,73 @@
+import cv2
+import numpy as np
+import sys
+from PIL import Image
+import pyscreenshot as ImageGrab
+
+# grab references to the global variables
+imageName = "croppedimage.png"
+if __name__ == '__main__':
+# part of the screen
+    #im = ImageGrab.grab(bbox=(10, 10, 510, 510)) # X1,Y1,X2,Y2
+    im = ImageGrab.grab()
+
+    # save image file
+    im.save(imageName)
+
+    cropping = False
+
+    x_start, y_start, x_end, y_end = 0, 0, 0, 0
+
+    image = cv2.imread(imageName)
+    oriImage = image.copy()
+
+
+    def mouse_crop(event, x, y, flags, param):
+        global x_start, y_start, x_end, y_end, cropping
+
+        # if the left mouse button was DOWN, start RECORDING
+        # (x, y) coordinates and indicate that cropping is being
+        if event == cv2.EVENT_LBUTTONDOWN:
+            x_start, y_start, x_end, y_end = x, y, x, y
+            cropping = True
+
+        # Mouse is Moving
+        elif event == cv2.EVENT_MOUSEMOVE:
+            if cropping == True:
+                x_end, y_end = x, y
+
+        # if the left mouse button was released
+        elif event == cv2.EVENT_LBUTTONUP:
+            # record the ending (x, y) coordinates
+            x_end, y_end = x, y
+            cropping = False # cropping is finished
+
+            refPoint = [(x_start, y_start), (x_end, y_end)]
+
+            if len(refPoint) == 2: #when two points were found
+                roi = oriImage[refPoint[0][1]:refPoint[1][1], refPoint[0][0]:refPoint[1][0]]
+                #cv2.imshow("Cropped", roi)
+                new_im = Image.fromarray(roi)
+                new_im.save(imageName)
+                sys.exit()
+
+
+    cv2.namedWindow("image",cv2.WINDOW_KEEPRATIO)
+    cv2.setMouseCallback("image", mouse_crop)
+    #cv2.setWindowProperty('image',cv2.WND_PROP_ASPECT_RATIO,cv2.WINDOW_KEEPRATIO)
+    cv2.setWindowProperty('image',cv2.WND_PROP_FULLSCREEN,cv2.WINDOW_FULLSCREEN)
+    while True:
+
+        i = image.copy()
+
+        if not cropping:
+            cv2.imshow("image", image)
+
+        elif cropping:
+            cv2.rectangle(i, (x_start, y_start), (x_end, y_end), (255, 0, 0), 2)
+            cv2.imshow("image", i)
+
+        cv2.waitKey(1)
+
+    # close all open windows
+    cv2.destroyAllWindows()
